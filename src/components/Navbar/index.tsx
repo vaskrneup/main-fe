62595773fe556fbaf3c22d76cbfa1db6f2c5afca
Bhaskar Neupane <39903236+vaskrneup@users.noreply.github.com>
2022-01-21 22:08:29 +0545

    Initial Final touches for intro and about me completed !!

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,39 +1,41 @@
+import { Section } from "../../pages/Home/components/Section";
 import { LogoMaker } from "../LogoMaker";
 import "./index.css";
 
 export const Navbar = () => {
     return (
-        <div className="navigation-section">
-            <nav className="navbar-container">
-                <div className="container">
-                    <div className="navbar">
-                        <div className="logo-section">
-                            <LogoMaker
-                                mainChar="V"
-                                name="askr"
-                            />
+        <Section>
+            <div className="navigation-section">
+                <nav className="navbar-container">
+                    <div className="container">
+                        <div className="navbar">
+                            <div className="logo-section">
+                                <LogoMaker
+                                    mainChar="V"
+                                    name="askr"
+                                />
+                            </div>
+                            <ul className="navigation-link">
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Projects</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
                         </div>
-                        <ul className="navigation-link">
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
+                    </div>
+                </nav>
+                <div className="navbar-bottom-detail left-line">
+                    <div className="vertical-line">
+                        &nbsp;
                     </div>
                 </div>
-            </nav>
-            <div className="navbar-bottom-detail left-line">
-                <span></span>
-                <div className="vertical-line">
-                    &nbsp;
+                <div className="navbar-bottom-detail right-line">
+                    <a href="mailto:abc@example.com">bhaskar@vaskrneup.com</a>
+                    <div className="vertical-line">
+                        &nbsp;
+                    </div>
                 </div>
             </div>
-            <div className="navbar-bottom-detail right-line">
-                <a href="mailto:abc@example.com">bhaskar@vaskrneup.com</a>
-                <div className="vertical-line">
-                    &nbsp;
-                </div>
-            </div>
-        </div>
+        </Section>
     )
 }
--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -6,24 +6,26 @@ export const Intro = () => {
     return (
         <div className="intro-section">
             <div className="container">
-                <div className="intro">
-                    <h4 className="my-name-title">
-                        Hi! my name is
-                    </h4>
-                    <h1 className="name">
-                        Bhaskar Neupane
-                    </h1>
-                    <h2 className="what-can-i-do">
-                        I build software solutions for your problems.
-                    </h2>
+                <div className="section-body">
+                    <div className="intro">
+                        <h4 className="my-name-title">
+                            Hi! my name is
+                        </h4>
+                        <h1 className="name section-title">
+                            Bhaskar Neupane
+                        </h1>
+                        <h2 className="what-can-i-do">
+                            I build software solutions for your problems.
+                        </h2>
 
-                    <p className="brief-intro">
-                        Hello! I'm a recent A-Levels graduate and a Full-Stack Developer based in Kathmandu, Nepal. Currenly, I am learning about Machine Learning and working as a Freelancer.
-                    </p>
+                        <p className="brief-intro">
+                            Hello! I'm a recent A-Levels graduate and a Full-Stack Developer based in Kathmandu, Nepal. Currenly, I am learning about Machine Learning and working as a Freelancer.
+                        </p>
 
-                    <a href="/apple" className="resume-btn">
-                        RESUME
-                    </a>
+                        <a href="/apple" className="resume-btn button">
+                            RESUME
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
