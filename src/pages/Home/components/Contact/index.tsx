883f4057d293baae594f54caac1a116c6e921634
Bhaskar Neupane <39903236+vaskrneup@users.noreply.github.com>
2022-01-22 00:45:27 +0545

    Interactions initial completed !!

--- src/pages/Home/components/Contact/index.tsx
+++ src/pages/Home/components/Contact/index.tsx
@@ -1,7 +1,25 @@
+import React, { FormEvent, useState } from "react";
 import { Section } from "../Section";
 import "./contact.css";
 
 export const Contact = () => {
+    const [formDetails, setFormDetails] = useState({
+        full_name: "",
+        email: "",
+        "message": ""
+    })
+
+    const handleInputValueChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        setFormDetails(prevValue => ({
+            ...prevValue,
+            [e.target.name]: e.target.value
+        }));
+    }
+    const handleFormSubmission = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+    }
+
     return (
         <Section>
             <div className="contact-container">
@@ -14,23 +32,28 @@ export const Contact = () => {
                         <div className="contact">
                             <div className="contact-main-content">
                                 <p>
-                                    Wether you have a question, want to talk about tech or project collaboration, or  just want to say hi. My inbox is always open. You can directly email me at bhaskar@vaskrneup.com or say hello using the following form. I&apos;ll get back to you as soon as possible!
+                                    Wether you have a question, want to talk about tech or project collaboration, or  just want to say hi. My inbox is always open. You can directly email me at <a href="mailto:bhaskar@vaskrneup.com" className="animated-line">bhaskar@vaskrneup.com</a> or say hello using the following form. I&apos;ll get back to you as soon as possible!
                                 </p>
                                 <div className="contact-form-container">
-                                    <form action="#">
+                                    <form action="#" onSubmit={handleFormSubmission}>
                                         <div className="name-email-container">
                                             <div className="input-container">
                                                 <label htmlFor="full-name">Full Name</label>
                                                 <input
+                                                    onChange={handleInputValueChange}
                                                     id="full-name"
                                                     type="text"
+                                                    name="full_name"
                                                     placeholder="Bhaskar Neupane"
                                                 />
                                             </div>
                                             <div className="input-container">
                                                 <label htmlFor="email">Email</label>
                                                 <input
+                                                    onChange={handleInputValueChange}
+                                                    id="email"
                                                     type="text"
+                                                    name="email"
                                                     placeholder="bhaskar@vaskrneup.com"
                                                 />
                                             </div>
@@ -39,8 +62,10 @@ export const Contact = () => {
                                             <div className="input-container">
                                                 <label htmlFor="message">Message</label>
                                                 <textarea
+                                                    onChange={handleInputValueChange}
                                                     placeholder="Lets have a chat!!"
                                                     id="message"
+                                                    name="message"
                                                     cols={30}
                                                     rows={10}
                                                 ></textarea>
