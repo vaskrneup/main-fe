3b8fa1049f743aeca8fb1f4bc0111d327e181bbe
Bhaskar Neupane <39903236+vaskrneup@users.noreply.github.com>
2022-01-26 11:33:01 +0545

    Overflow issue fixed && better scorllbar

--- src/pages/project/ProjectDetailView/index.tsx
+++ src/pages/project/ProjectDetailView/index.tsx
@@ -8,6 +8,7 @@ import axios from "axios";
 import remarkGfm from 'remark-gfm'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus as codeBlockTheme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import "./project-detail-view.css"
 
 
 export const ProjectDetailView = (props?: optionalProjectViewPropsDatatype | projectViewPropsDatatype) => {
